4365a6d5f75c7cb81c5b53140baf4fb0dd8aebbb
Abreu Fernandez <pablo.abreufernandez@cencosud.cl>
2018-08-27 09:00:13 -0300

    save pc

---
+++ README.md
@@ -0,0 +1,11 @@
+# Notas:
+
+Este es un pequeño servidor de express listo para ejecutarse y servir la carpeta public en la web.
+
+La idea del proyecto es crear un chat usando Socket.IO
+
+Recuerden que deben de reconstruir los módulos de node con el comando
+
+```
+npm install
+```
---
+++ server/classes/usuarios.js
@@ -0,0 +1,24 @@
+'use-strict';
+
+  /************************
+ *       Ej._USUARIO       *
+ *   ===================    *
+ * {                        *
+ *  id: 'Akhsggd-ewwgdy7h', *
+ *  nombre: 'Pablo',        *
+ *  sala: 'Videojuegos'     *
+ * }                        *
+ *                          *
+  **************************/
+
+class Usuarios {
+
+  constructor(){
+    this.personas = [];
+  }
+
+}
+
+module.exports = {
+  Usuarios
+}
